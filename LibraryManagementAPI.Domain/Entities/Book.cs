7ab7519156ae7e6a41904400ace8cb9b38727cdb
shuvomahamud <shuvomahamud@gmail.com>
2024-06-13 20:26:39 -0400

    domain: Add domain entities and interfaces

---
+++ LibraryManagementAPI.Domain/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryManagementAPI.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string CoverImagePath { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Category { get; set; }
+        public string ISBN { get; set; }
+        public int PageCount { get; set; }
+        public bool IsAvailable { get; set; }
+        public int Copies { get; set; }
+    }
+}
+
